e31d6fb9db21a82d352b873f261c5c4f8e8f23da
Ibrahim <ibrahim.haji.3595@gmail.com>
2025-10-06 12:43:31 +0530

    Initialized async main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::Surreal;
+use serde::{Serialize, Deserialize};
+use dialoguer::{Input, Select};
+use rpassword::read_password;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct User {
+    username: String,
+    password: String,
+    id: Option<surrealdb::RecordId>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct JournalEntry {
+    id: Option<surrealdb::RecordId>,
+    user: String,
+    title: String,
+    content: String, 
+}
+
+#[tokio::main]
+async fn main() {
+    println!("Wrlcome to secure Journal app..");
 }
