3f24295dd97d41b63d9789ef93bc7c3b9774dd9a
Ibrahim <ibrahim.haji.3595@gmail.com>
2025-11-14 16:50:27 +0530

    major update to db type from Client to Mem allowing users to store their entires locally

--- src/db.rs
+++ src/db.rs
@@ -1,30 +1,9 @@
-use surrealdb::Surreal;
-use surrealdb::engine::local::{Mem, Db};
 use anyhow::Result;
+use surrealdb::Surreal;
+use surrealdb::engine::local::{Db, Mem};
 
 pub async fn connect() -> Result<Surreal<Db>> {
     let db = Surreal::new::<Mem>(()).await?;
     println!("Connected to in-memory SurrealDB (Mem engine).");
     Ok(db)
 }
-
-
-
-//reduntant code needed for later...dont del
-// use surrealdb::Surreal;
-// use surrealdb::engine::remote::ws::{Client, Ws};
-// use surrealdb::opt::auth::Root;
-
-// pub async fn connect() -> Result<Surreal<Db>> {
-//     let db = Surreal::new::<File>("secure_journal.db").await.unwrap();
-//     // let _ = db
-//     //     .signin(Root {
-//     //         username: "root",
-//     //         password: "secret",
-//     //     })
-//     //     .await;
-//     // db.use_ns("journal").use_db("database").await.unwrap();
-
-//     Ok(db)
-// }
-//
