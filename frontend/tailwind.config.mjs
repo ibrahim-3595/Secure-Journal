a23de59d6caad071aac124106a7ef1c50a5642fd
Ibrahim <ibrahim.haji.3595@gmail.com>
2025-11-17 17:22:48 +0530

    partially fixed bugs in tailwind env, styling implement but not reflected

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/**/*.rs",
+    "./src/**/*.html",
+    "./src/**/*.jsx",
+    "./src/**/*.tsx",
+    "./**/*.rs"
+  ],
+};
+
+export default config;
