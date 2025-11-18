dd024a47b178142c25415019282faf52173aa272
Ibrahim <ibrahim.haji.3595@gmail.com>
2025-11-18 11:50:46 +0530

    integrated file-backend storage allowing users to store their data in a db file locally, partial fix

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,12 +1,15 @@
-use surrealdb::Surreal;
-use surrealdb::engine::local::{Db, Mem};
 use anyhow::Result;
+use std::fs::File;
+use surrealdb::Surreal;
+use surrealdb::engine::local::{Db, Mem, RocksDb};
 
 pub async fn connect() -> Result<Surreal<Db>> {
-    let db = Surreal::new::<Mem>(()).await?;
+    // let db = Surreal::new::<Mem>(()).await?;
+    let db =
+        Surreal::new::<RocksDb>("/home/ibrahim/code/rust_programming/secure_journal/mydb").await?;
 
     db.use_ns("app").use_db("journal").await?;
 
-    println!("Connected to in-memory SurrealDB");
+    println!("Connected to file-backend SurrealDB");
     Ok(db)
 }
