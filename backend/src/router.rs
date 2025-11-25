69189ed0550fa80b2b91d7ac9e2756e4572efb70
Ibrahim <ibrahim.haji.3595@gmail.com>
2025-11-25 16:49:39 +0530

    add cors layer in router

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,15 +1,18 @@
 use std::sync::Arc;
 use axum::{Router, routing::post};
-
+use tower_http::cors::{CorsLayer, Any};
 use crate::AppState;
-use crate::auth::http::{
-    login_handler::api_login,
-    signup_handler::api_signup,
-};
+use crate::auth::http::{login_handler::api_login, signup_handler::api_signup};
 
 pub fn create_router(state: Arc<AppState>) -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_headers(Any)
+        .allow_methods(Any);
+
     Router::new()
         .route("/api/login", post(api_login))
         .route("/api/signup", post(api_signup))
         .with_state(state)
+        .layer(cors)
 }
