6eaf267e495bdb0d2cdf2b70283c7563856ebf88
Ibrahim <98814268+ibrahim-3595@users.noreply.github.com>
2026-01-20 17:31:36 +0530

    changes being resolved

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -4,6 +4,6 @@ use anyhow::Result;
 pub type DbPool = SqlitePool;
 
 pub async fn connect() -> Result<DbPool> {
-    let pool = SqlitePool::connect("sqlite:///home/ibrahim/code/rust_programming/secure_journal/backend/data/journal.db").await?;
+    let pool = SqlitePool::connect("urlsqlite:///Users/ibrahimhaji/code/rust_programming/secure_journal/backend/data/journal.db").await?;
     Ok(pool)
 }
